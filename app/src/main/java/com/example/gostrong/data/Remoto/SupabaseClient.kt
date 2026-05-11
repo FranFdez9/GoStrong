b62fea908fd3db126c943c399c75825422389f9c
Francisco José <fraanfernandezz9@gmail.com>
2026-05-11 11:13:01 +0200

    Update SupabaseClient.kt

--- app/src/main/java/com/example/gostrong/data/Remoto/SupabaseClient.kt
+++ app/src/main/java/com/example/gostrong/data/Remoto/SupabaseClient.kt
@@ -7,8 +7,8 @@ import io.github.jan.supabase.postgrest.Postgrest
 
 object Supabase {
     val client: SupabaseClient = createSupabaseClient(
-        supabaseUrl = "https://gvfnihpkuycobbqaklxh.supabase.co",
-        supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2Zm5paHBrdXljb2JicWFrbHhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NjMxOTMsImV4cCI6MjA5MjIzOTE5M30.qmTL3yDwKD1RtItzlj1qK6Gl-XQ9BZ0CIZou_UbfhN0"
+        supabaseUrl = "PUT YOUR SUPABASE URL HERE",
+        supabaseKey = "PUT YOUR SUPABASE KEY HERE"
     ) {
         install(Auth)
         install(Postgrest)
